b6c760f728bc682fb3f6a5fecaf4c8fb4bdc4b9b
Ross Brown <ross@cremalab.com>
2013-09-03 12:46:56 -0500

    Adding Home and Contact controllers so Rob can get crankin. see #1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CremalabCom::Application.config.secret_key_base = '1bfbf45147afe82fc24503cb92e0ad29d8280aad754842f1380d43317f7eb11f4d5de425791a6e69633a840d817611c80d17d86fce38717420aea36af6756acf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CremalabCom::Application.config.session_store :cookie_store, key: '_cremalab_com_session'
