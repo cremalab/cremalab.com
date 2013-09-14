8ae394efc01b0460dae3af88ec2a52a26025dfe9
matao <mattowens11@gmail.com>
2013-09-14 09:23:13 -0500

    Scaffolded the tests for blog

--- test/controllers/blogs_controller_test.rb
+++ test/controllers/blogs_controller_test.rb
@@ -4,4 +4,24 @@ class BlogsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "index" do
+
+  end
+
+  test "create" do
+
+  end
+
+  test "show" do
+
+  end
+
+  test "update" do
+
+  end
+
+  test "destoy" do
+
+  end
 end
