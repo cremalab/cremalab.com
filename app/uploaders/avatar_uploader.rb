fd5ea02ee5077a06f04c0aed735c9ce5b1e91843
Ross Brown <ross@cremalab.com>
2013-09-16 16:58:23 -0500

    Added carrierwave initializer. #22

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -11,16 +11,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
   storage :fog
 
 
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['S3_ACCESS_KEY'],         # required
-    :aws_secret_access_key  => ENV['S3_SECRET'],             # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = 'cremalab.com'                          # required
-  config.fog_public     = true                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
