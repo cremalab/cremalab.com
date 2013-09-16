c2036dd161c2c9c7e1f0f45d8c8e980fba71ff9d
Ross Brown <ross@cremalab.com>
2013-09-16 16:09:44 -0500

    5 posts per page on all index pages. closes #20
    also change the route from blogs to blog. because.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,15 +2,15 @@ class BlogsController < ApplicationController
   def index
     if blog_user
       if current_user
-        @blogs = blog_user.blogs.order("published_at DESC")
+        @blogs = blog_user.blogs.order("published_at DESC").page(params[:page]).per(5)
       else
-        @blogs = blog_user.blogs.published.order("published_at DESC")
+        @blogs = blog_user.blogs.published.order("published_at DESC").page(params[:page]).per(5)
       end
     else
       if current_user
-        @blogs = Blog.all.order("published_at DESC")
+        @blogs = Blog.all.order("published_at DESC").page(params[:page]).per(5)
       else
-        @blogs = Blog.published.order("published_at DESC")
+        @blogs = Blog.published.order("published_at DESC").page(params[:page]).per(5)
       end
     end
     render :index, status: 200
