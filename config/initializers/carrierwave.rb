ebe619bc4f31c54e37b4ba772a7ee21dfaf18183
Ross Brown <ross@cremalab.com>
2013-09-16 17:22:39 -0500

    set up s3 correctly. #22

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['S3_ACCESS_KEY'],         # required
     :aws_secret_access_key  => ENV['S3_SECRET'],             # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    :region                 => 'us-east-1',                  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'cremalab.com'                          # required
+  config.fog_directory  = 'cremalab'                             # required
   config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
