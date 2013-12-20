06c7e04ecc8a7b90d50846a997e070be7cad0c56
Ross Brown <ross@cremalab.com>
2013-12-20 14:37:48 -0600

    work stuff precompiled.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ CremalabCom::Application.configure do
 
   config.assets.paths << Rails.root.join('fonts')
 
-  config.assets.precompile += %w( .svg .eot .woff .ttf print.css admin.css admin.js)
+  config.assets.precompile += %w( .svg .eot .woff .ttf print.css admin.css admin.js require.js *templates/* *works/*)
 
   config.assets.initialize_on_precompile = false
 
