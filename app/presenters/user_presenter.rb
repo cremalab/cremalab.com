45a78d86c017a0ab6075d2ec864d894b6fe2cff5
Ross Brown <ross@cremalab.com>
2014-01-03 14:44:43 -0600

    kind of another typo

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -6,10 +6,10 @@ class UserPresenter < BasePresenter
     @object.full_name
   end
   def blog_link
-    if @object.profile.bio_url
-      url = @object.profile.bio_url
-    else
+    if @object.profile.bio_url.blank?
       url = h.user_blogs_path(@object)
+    else
+      url = @object.profile.bio_url
     end
     h.link_to "Meet #{first_name}", url, class: "button sub-outline"
   end
