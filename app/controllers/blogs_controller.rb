1a20cdb5ae061c840578288a63521ac6df21f0d1
Ross Brown <ross@cremalab.com>
2014-01-06 12:33:12 -0600

    upping blog per page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,9 +3,9 @@ class BlogsController < ApplicationController
   def index
     if blog_user
       @user  = blog_user
-      @blogs = blog_user.blogs.active.order("published_at DESC").page(params[:page]).per(2)
+      @blogs = blog_user.blogs.active.order("published_at DESC").page(params[:page]).per(10)
     else
-      @blogs = Blog.active.order("published_at DESC").page(params[:page]).per(2)
+      @blogs = Blog.active.order("published_at DESC").page(params[:page]).per(10)
     end
     render :index, status: 200
   end
