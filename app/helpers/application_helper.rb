f9165fe70f544ddac423e1f724f1867e806c1aa0
Ross Brown <ross@cremalab.com>
2014-01-06 11:54:55 -0600

    map_url helper for iOS/Google maps url generation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
   def title(page_title)
     content_for :title, " - #{page_title.to_s}"
   end
+  def map_url(query)
+    agent = request.env["HTTP_USER_AGENT"]
+    query = query.split(" ")
+    query = query.join("+")
+    if agent.include? "Apple"
+      "http://maps.apple.com/?q=#{query}"
+    else
+      "https://maps.google.com/maps?q=#{query}"
+    end
+  end
 end
